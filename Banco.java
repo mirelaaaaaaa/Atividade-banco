7a87d334d1f8487abe459d89274679a1776204d0
Grupo1 <lss78@aluno.ifal.edu.br>
2024-08-26 10:47:30 -0300

    Continuação da atividade

--- Banco.java
+++ Banco.java
@@ -1,5 +1,14 @@
 public class Banco {
     
     String nome = "Pesadelo do sistema";
-    
+
+    void criar_conta(){
+        System.out.println("Criando nova conta");
+    }
+    void encontrar_conta(){
+        System.out.println();
+    }
+    void transferir(){
+        System.out.println();
+    }
 }
--- Contas.java
+++ Contas.java
@@ -1,3 +1,36 @@
 public class Contas {
-    Contas conta;
+
+    //atributos
+
+    int numero;
+    String titular;
+    double saldo;
+    
+
+    //CONSTRUTOR
+    public Contas (int numero, String titular, double saldo){
+     this.numero = numero;
+     this.titular = titular;
+     this.saldo = saldo;
+     }
+
+     public double getSaldo(){
+        return saldo;                                     
+     }
+
+    //métodos
+    public void depositar(double valor){
+        System.out.println("Adicionando valor ao saldo" );
+    }
+    public void sacar(double valor ){
+        if(valor <= saldo){
+        double subtra = valor - saldo;
+        System.out.println(subtra);
+        }else {
+            System.out.println("Saldo insuficiente");
+        }
+    }
+     public double getVerificar_saldo(){
+        return saldo;
+    }
 }
